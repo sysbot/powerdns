ae195269a1b9f41081a51074028fd263d794b663
Bao Nguyen <bao@ooyala.com>
2014-05-15 13:42:46 -0700

    adding back ooyala-mariadb

--- Berksfile
+++ Berksfile
@@ -3,6 +3,7 @@ site :opscode
 metadata
 
 cookbook 'mariadb', :path => "#{ENV['CHEF_REPO']}/cookbooks/mariadb"
+cookbook 'ooyala-mariadb', :path => "#{ENV['CHEF_REPO']}/site-cookbooks/ooyala-mariadb"
 cookbook 'database', :path =>  "#{ENV['CHEF_REPO']}/cookbooks/database"
 cookbook 'resolvconf', :path =>  "#{ENV['CHEF_REPO']}/cookbooks/resolvconf"
 cookbook 'sqlite'
--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,12 @@
 name             "powerdns"
-maintainer       "Opscode, Inc."
-maintainer_email "cookbooks@opscode.com"
+maintainer       "Bao Nguyen"
+maintainer_email "team-ops@ooyala.com"
 license          "Apache 2.0"
-description      "Installs/Configures pdns"
+description      "Installs/Configures PowerDNS"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.4.1"
 depends "mariadb"
+depends "ooyala-mariadb"
 depends "sqlite"
 
 depends "build-essential"
